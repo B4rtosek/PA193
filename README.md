c714e8c161004d30c71942813aa0de1ad241f00f
PrisionMike <su.sh2396@gmail.com>
2022-02-26 15:59:34 +0100

    some improvements

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 - Bech32m encoding
 - Test vectors are in the file `testvectors.txt` in Pythonic format.
 - Remember to sign your commits. Prefer GPG..
-- No use strucurising test vectors as JSON since there exists no JSON parser. Will right a few tests using the `serde_json` binary but someone will have to replace that subsequently.
+- No use strucurising test vectors as JSON since there exists no JSON parser in standard library. Will right a few tests using the `serde_json` binary but someone will have to replace that subsequently.
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,17 @@
 
 fn valideh( teststr: &str ) -> &str {
     /*
+    ===THIS IS PART OF THE MAIN CODE. THIS FUNCTION WILL BE USED INSIDE MAIN (main.rs) FILE
+    FOR FUNCTIONALITY.===
+
     The entire logic of validating bech32m goes here.
     */
     if teststr.eq("A1LQFN3A") { // The very first string in the list.
         "VALID"
-    } else { "INVALID" }
+    } else { panic!("INVALID")}
 }
+
+//  ========== TESTS START HERE ==========
 
 #[cfg(test)]
 mod tests {
@@ -17,7 +22,9 @@ mod tests {
 
     #[test]
     fn isvalid() {
-
+        /*
+        The test to test valid bech32m vectors.
+        */
         let fileasstr = fs::read_to_string(".\\validbech32m.json").expect(" Error parsing test file as string");
         // The current directory for testing would be the root directory (which contains the src folder)
         // Thus the file has been copied there as well.
@@ -26,5 +33,14 @@ mod tests {
         // dbg!(jsonval["VALID_BECH32M"]);
         assert_eq!("VALID",valideh(jsonval["VALID_BECH32M"][1].as_str().unwrap()));
 
+    }
+
+    #[test]
+    #[should_panic( expected = "INVALID")]
+    fn isinvalid() {
+        /* Test the invalid bech32m vectors here. The code should panic if it finds (for now) or produces (later)
+        and invalid word.
+        The panic should be: panic!("INVALID")
+        */
     }
 }
