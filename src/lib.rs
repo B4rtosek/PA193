18b80d6329cf58b14335d203e7a0c5b69ac2278a
Petr Sopf <trsak1@seznam.cz>
2022-04-08 21:07:04 +0200

    Format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-use std::{fs, result, io::Error, io::ErrorKind};
-use serde_json::Value;      // Hardcode the test vectors in the code and remove this.
+use serde_json::Value;
+use std::{fs, io::Error, io::ErrorKind, result}; // Hardcode the test vectors in the code and remove this.
 
-
-const BECH32M_CONST : usize = 0x2bc830a3;
-const DATA_LUT : [&'static str; 4] = ["qpzry9x8","gf2tvdw0","s3jn54kh","ce6mua7l"];
+const BECH32M_CONST: usize = 0x2bc830a3;
+const DATA_LUT: [&'static str; 4] = ["qpzry9x8", "gf2tvdw0", "s3jn54kh", "ce6mua7l"];
 
 pub struct ValidationResponse {
     result: bool,
-    reason: String
+    reason: String,
 }
 
 pub fn data_to_int(data: &str) -> Vec<usize> {
@@ -16,7 +15,7 @@ pub fn data_to_int(data: &str) -> Vec<usize> {
     for i in dataiter {
         for j in 0..4 {
             if let Some(v) = DATA_LUT[j].find(i) {
-                let val = (8*j + v) as usize;
+                let val = (8 * j + v) as usize;
                 dataint.push(val);
             };
         }
@@ -56,11 +55,11 @@ pub fn verify_checksum(hrp: &str, data: &str) -> bool {
     res == BECH32M_CONST
 }
 
-pub fn create_checksum( hrp: &str, data: &str) -> Vec<usize> {
+pub fn create_checksum(hrp: &str, data: &str) -> Vec<usize> {
     let mut values: Vec<usize> = hrp_expand(hrp);
     let mut data = data_to_int(data);
     values.append(&mut data);
-    let mut zerosvec: Vec<usize> = vec![0,0,0,0,0,0];
+    let mut zerosvec: Vec<usize> = vec![0, 0, 0, 0, 0, 0];
     values.append(&mut zerosvec);
     let polymod_res = polymod(&values) ^ BECH32M_CONST;
     let mut checksum_vec: Vec<usize> = Vec::with_capacity(6);
@@ -70,10 +69,10 @@ pub fn create_checksum( hrp: &str, data: &str) -> Vec<usize> {
     checksum_vec
 }
 
-pub fn hrp_expand( hrp: &str) -> Vec<usize> {
+pub fn hrp_expand(hrp: &str) -> Vec<usize> {
     let hrpiter = hrp.chars();
-    let hrpiter2 = hrpiter.clone();     // A better way would be to borrow the iterator into the loop but I am not in the mood to do it rn.
-    // The clone is required as the iterator is consumed upon use. More specifically, it moves into the for loop when being called as it is.
+    let hrpiter2 = hrpiter.clone(); // A better way would be to borrow the iterator into the loop but I am not in the mood to do it rn.
+                                    // The clone is required as the iterator is consumed upon use. More specifically, it moves into the for loop when being called as it is.
     let mut hrpx: Vec<usize> = Vec::new();
     for c in hrpiter {
         hrpx.push((c as usize) >> 5);
@@ -90,8 +89,11 @@ pub fn encode(hrp: &str, data: &str) -> Result<String, Error> {
     Unsafe function. Doesn't check the validity of hrp and data strings.
     */
     let hrpvalres = hrp_valideh(hrp);
-    if !hrpvalres.result{
-        return Err(std::io::Error::new(ErrorKind::Other,format!("Error with HRP: {}",hrpvalres.reason)));
+    if !hrpvalres.result {
+        return Err(std::io::Error::new(
+            ErrorKind::Other,
+            format!("Error with HRP: {}", hrpvalres.reason),
+        ));
     }
     let checksum = create_checksum(hrp, data);
     let dataint = data_to_int(data);
@@ -112,57 +114,70 @@ pub fn encode(hrp: &str, data: &str) -> Result<String, Error> {
     let mut encoded_string = String::new();
     encoded_string.push_str(hrp);
     encoded_string.push('1');
-    
     for i in combined {
-        let string_index = (i/8) as usize;
+        let string_index = (i / 8) as usize;
         let holder_string = DATA_LUT[string_index];
-        encoded_string.push(holder_string.chars().nth(i - 8*string_index).unwrap());
+        encoded_string.push(holder_string.chars().nth(i - 8 * string_index).unwrap());
     }
 
     Ok(encoded_string)
 }
 
-fn hrp_valideh( hrp: &str ) -> ValidationResponse {
+fn hrp_valideh(hrp: &str) -> ValidationResponse {
     let hrp_len = hrp.len();
 
     let mut response = String::new();
     if hrp_len >= 1 && hrp_len <= 83 {
-
         let hrpiter = hrp.chars();
-        let mut casecheckflag: usize = 0;      //0 = not set. 1 = uppercase chars. 2 = lowercase chars.
-        for i in hrpiter{
+        let mut casecheckflag: usize = 0; //0 = not set. 1 = uppercase chars. 2 = lowercase chars.
+        for i in hrpiter {
             // dbg!(&i);
             // Valid character range check
-            if i >= '\u{0021}' && i <= '\u{007E}' {     // ASCII 33 - 126 translation into UTF-8
+            if i >= '\u{0021}' && i <= '\u{007E}' {
+                // ASCII 33 - 126 translation into UTF-8
                 //Mix case check
-                if i.is_lowercase(){
+                if i.is_lowercase() {
                     if casecheckflag == 1 {
                         response = "INVALID: MIX CASE HRP".to_owned();
-                        return ValidationResponse{result: false, reason: response}
+                        return ValidationResponse {
+                            result: false,
+                            reason: response,
+                        };
                     }
                     casecheckflag = 2;
-                } else if i.is_uppercase(){
+                } else if i.is_uppercase() {
                     if casecheckflag == 2 {
                         response = "INVALID: MIX CASE HRP".to_owned();
-                        return ValidationResponse{result: false, reason: response}
+                        return ValidationResponse {
+                            result: false,
+                            reason: response,
+                        };
                     }
                     casecheckflag = 1;
                 }
             } else {
                 response = "INVALID: HRP CHARACTERS INVALID".to_owned();
-                return ValidationResponse{result: false, reason: response}
+                return ValidationResponse {
+                    result: false,
+                    reason: response,
+                };
             }
         }
     } else {
         response = "INVALID: HRP LENGTH OUT OF RANGE".to_owned();
-        return ValidationResponse{result: false, reason: response}
+        return ValidationResponse {
+            result: false,
+            reason: response,
+        };
     }
 
-    ValidationResponse { result: true, reason: response }
-
+    ValidationResponse {
+        result: true,
+        reason: response,
+    }
 }
 
-pub fn valideh( teststr: &str ) -> ValidationResponse {
+pub fn valideh(teststr: &str) -> ValidationResponse {
     /*
     ===THIS IS PART OF THE MAIN CODE. THIS FUNCTION WILL BE USED INSIDE MAIN (main.rs) FILE
     FOR FUNCTIONALITY.===
@@ -178,34 +193,35 @@ pub fn valideh( teststr: &str ) -> ValidationResponse {
         // Length check.
         let teststrlen = teststr.len();
         if teststrlen <= 90 && teststrlen > 0 {
-        
-        let hrp: &str;
-        let datapart: &str;
-        
-        /*
-        Better logic exists using rsplit_once()
-        */
-        let mut separator: usize = 0;
-        let teststriter = teststr.chars().enumerate();
-        for i in teststriter {
-            let (ind, val) = i;
-            if val == '1' {
-                separator = ind;
+            let hrp: &str;
+            let datapart: &str;
+
+            /*
+            Better logic exists using rsplit_once()
+            */
+            let mut separator: usize = 0;
+            let teststriter = teststr.chars().enumerate();
+            for i in teststriter {
+                let (ind, val) = i;
+                if val == '1' {
+                    separator = ind;
+                }
             }
-        }
 
+            hrp = &teststr[..separator];
+            datapart = &teststr[separator + 1..];
 
-        hrp = &teststr[..separator];
-        datapart = &teststr[separator+1..];
+            // dbg!(&hrp);
+            // dbg!(&datapart);
+            println!(
+                "🔛 {} is split into {} as hrp and {} as data",
+                teststr, hrp, datapart
+            );
 
-        // dbg!(&hrp);
-        // dbg!(&datapart);
-        println!("🔛 {} is split into {} as hrp and {} as data", teststr, hrp, datapart);
-
-        let hrp_res = hrp_valideh(hrp);
-        if hrp_res.result == false {
-            return hrp_res
-        }
+            let hrp_res = hrp_valideh(hrp);
+            if hrp_res.result == false {
+                return hrp_res;
+            }
 
             /*
             The data part, which is at least 6 characters long and only consists of alphanumeric characters excluding "1", "b", "i", and "o"[4].
@@ -216,18 +232,20 @@ pub fn valideh( teststr: &str ) -> ValidationResponse {
             let datapart = datapart.as_str();
             if datapart.len() < 6 {
                 response = "INVALID: DATAPART LENGTH TOO SMALL".to_owned();
-                println!("{}",response);
-                return ValidationResponse{result: false, reason: response}
+                println!("{}", response);
+                return ValidationResponse {
+                    result: false,
+                    reason: response,
+                };
             } else {
                 let datachars = datapart.chars();
                 for i in datachars {
                     if i.is_ascii_alphanumeric() && i != '1' && i != 'b' && i != 'i' && i != 'o' {
-                        
                         /*
                         Data character validity testing ends here. The tests to compute and test the checksums should follow.
                         */
                         let _res;
-                        if verify_checksum(hrp, datapart) { 
+                        if verify_checksum(hrp, datapart) {
                             response = "VALID".to_owned();
                             _res = true;
                         } else {
@@ -235,26 +253,40 @@ pub fn valideh( teststr: &str ) -> ValidationResponse {
                             _res = false;
                         }
 
-                        return ValidationResponse{result: _res, reason: response};
+                        return ValidationResponse {
+                            result: _res,
+                            reason: response,
+                        };
                     } else {
                         response = "INVALID: INVALID CHARACTERS IN DATA".to_owned();
-                        println!("{}",response);
-                        return ValidationResponse{ result: false, reason: response}                 
+                        println!("{}", response);
+                        return ValidationResponse {
+                            result: false,
+                            reason: response,
+                        };
                     }
                 }
             }
-                    
         } else {
             response = "INVALID: LENGTH OUT OF RANGE".to_owned();
-            println!("{}",response);
-            return ValidationResponse{ result: false, reason: response}
+            println!("{}", response);
+            return ValidationResponse {
+                result: false,
+                reason: response,
+            };
         }
     } else {
         response = "INVALID: SEPARATOR NOT FOUND".to_owned();
-        println!("{}",response);
-        return ValidationResponse{ result: false, reason: response}
+        println!("{}", response);
+        return ValidationResponse {
+            result: false,
+            reason: response,
+        };
     }
-    ValidationResponse{ result: false, reason: "YOU SHOULDNT BE HERE".to_owned() }
+    ValidationResponse {
+        result: false,
+        reason: "YOU SHOULDNT BE HERE".to_owned(),
+    }
 }
 
 //  ========== TESTS START HERE ==========
